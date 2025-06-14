66b572fa66bf1bf44c60518c376967157a9be80a
Aszanat <zuziakaczorek125@gmail.com>
2025-06-14 23:08:39 +0200

    Prevent racing conditions (in creatin g new short urls). I never liked this sport anyway.

--- url_api/api/models.py
+++ url_api/api/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# This is apparently Google Chrome's maximum URL length. Do we need more? (The answer is, we might, but DO WE?)
+url_max_len = 2048
 
 class Url(models.Model):
-    full_url = models.CharField(max_length=255, unique=True)
+    full_url = models.CharField(max_length=url_max_len, unique=True)
     short_url = models.CharField(max_length=63, unique=True)
--- url_api/api/views.py
+++ url_api/api/views.py
@@ -1,10 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.exceptions import ParseError, ValidationError, NotFound
-from .models import Url
+from .models import Url, url_max_len
 from .serializers import UrlSerializer
 from validators import url
 from uuid import uuid4
+from django.db import IntegrityError, transaction
+
+from random import choice
 
 @api_view(['GET'])
 def ApiOverview(request):
@@ -30,6 +33,9 @@ def new_url(request):
 
     if not url(new_url):
         raise ValidationError("Invalid url provided")
+    
+    if len(new_url) > url_max_len:
+        raise ValidationError("Long URL is too long to serialize")
 
     url_object = {"full_url" : new_url}
 
@@ -37,21 +43,26 @@ def new_url(request):
         old_url = Url.objects.get(**url_object)
         return Response(old_url.short_url)
     
-    short_url = str(uuid4()).replace('-','')[:8]
-    short_url_object = {"short_url": create_url_from_short(short_url)}
-
-    while Url.objects.filter(**short_url_object).exists():
+    while(True):
         short_url = str(uuid4()).replace('-','')[:8]
         short_url_object = {"short_url": create_url_from_short(short_url)}
+        try:
+            print("Transaction start!")
+            with transaction.atomic():
+                if Url.objects.filter(**short_url_object).exists():
+                    continue
+                else:
+                    url_object.update(short_url_object)
+                    url_pair = UrlSerializer(data=url_object)
 
-    url_object.update(short_url_object)
-    url_pair = UrlSerializer(data=url_object)
+                    if not url_pair.is_valid():
+                        raise serializers.ValidationError("This data could not have been serialized")
+                    url_pair.save()
 
-    if not url_pair.is_valid():
-        raise serializers.ValidationError("This data could not have been serialized")
-    url_pair.save()
-
-    return Response(short_url_object["short_url"])
+                    return Response(short_url_object["short_url"])
+        except IntegrityError:
+            continue
+    
 
 @api_view(['GET'])
 def find_url(request):
