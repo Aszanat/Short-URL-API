354eccc79bf48585d7e1e3514ccdf298bcae4de4
Aszanat <zuziakaczorek125@gmail.com>
2025-06-14 22:24:31 +0200

    Make URL fields unique

--- url_api/api/models.py
+++ url_api/api/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Url(models.Model):
-    full_url = models.CharField(max_length=255)
-    short_url = models.CharField(max_length=63)
+    full_url = models.CharField(max_length=255, unique=True)
+    short_url = models.CharField(max_length=63, unique=True)
